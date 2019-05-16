d9b8bf0a236b44447bdacdfb23ffa2acc9d997be
Guangzhan <fuzhan713@gmail.com>
2019-05-16 13:16:08 +0800

    home
    Login

--- app/home/views.py
+++ app/home/views.py
@@ -1,8 +1,18 @@
 # -*- coding: utf-8 -*-
 
 from . import home
-from flask import render_template
+from flask import render_template, redirect, url_for
 
 @home.route("/")
 def index():
     return render_template("home/index.html")
+
+
+@home.route("/login/")
+def login():
+    return render_template("home/login.html")
+
+
+@home.route("/logout/")
+def logout():
+    return redirect(url_for("home.login"))
