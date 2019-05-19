b5104eabe337282798226f4884b0277ade54cb25
Guangzhan <fuzhan713@gmail.com>
2019-05-19 20:02:02 +0800

    view
    视图

--- app/home/views.py
+++ app/home/views.py
@@ -3,6 +3,7 @@
 from . import home
 from flask import render_template, redirect, url_for
 
+
 @home.route("/")
 def index():
     return render_template("home/index.html")
@@ -21,3 +22,29 @@ def logout():
 @home.route("/regist/")
 def regist():
     return render_template("home/regist.html")
+
+
+@home.route("/user/")
+def user():
+    return render_template("home/user.html")
+
+
+@home.route("/pwd/")
+def pwd():
+    return render_template("home/pwd.html")
+
+
+
+@home.route("/comments/")
+def comments():
+    return render_template("home/comments.html")
+
+
+@home.route("/loginlog/")
+def loginlog():
+    return render_template("home/loginlog.html")
+
+
+@home.route("/moviecol/")
+def moviecol():
+    return render_template("home/moviecol.html")
